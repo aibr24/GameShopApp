02252a57e0eb3642d3ad8c7000dc18452b2b7c0b
aibr24 <aliibrahim4793@gmail.com>
2020-08-17 19:36:28 +0300

    cleanup

--- components/Home/index.js
+++ components/Home/index.js
@@ -5,27 +5,29 @@ import { HomePageBackground, TopView } from "./styles";
 
 const Home = ({ navigation }) => {
   return (
-    <HomePageBackground
-      source={{
-        uri:
-          "https://s1.econotimes.com/assets/uploads/20190120e168eca74623a62b0_th_1024x0.jpg",
-      }}
-    >
-      <TopView>
-        <Button block dark onPress={() => navigation.navigate("Games")}>
-          <Text>Games</Text>
-        </Button>
-        <Button
-          block
-          dark
-          onPress={() => {
-            navigation.navigate("Publisher");
-          }}
-        >
-          <Text>Publishers</Text>
-        </Button>
-      </TopView>
-    </HomePageBackground>
+    <Container>
+      <HomePageBackground
+        source={{
+          uri:
+            "https://s1.econotimes.com/assets/uploads/20190120e168eca74623a62b0_th_1024x0.jpg",
+        }}
+      >
+        <TopView>
+          <Button block dark onPress={() => navigation.navigate("Games")}>
+            <Text>Games</Text>
+          </Button>
+          <Button
+            block
+            dark
+            onPress={() => {
+              navigation.navigate("Publisher");
+            }}
+          >
+            <Text>Publishers</Text>
+          </Button>
+        </TopView>
+      </HomePageBackground>
+    </Container>
   );
 };
 
